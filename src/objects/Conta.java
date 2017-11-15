57b83bb9d6f5cde0259a14d258a2797e61c0d2d3
Projeto - DEV <lokluis@gmail.com>
2017-11-15 21:16:32 -0200

    Adicionando comentário a metodos

--- src/objects/Conta.java
+++ src/objects/Conta.java
@@ -2,6 +2,7 @@ package objects;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 /**
  * 
  * @author Luis Fernando
@@ -17,48 +18,49 @@ public abstract class Conta {
 	private String tipo;
 	private LocalDate dataCriacao;
 	private LocalDateTime dataTransacao;
-	
-	private enum Operacao{
-		SAQUE,
-		DEPOSITO,
-		TRANSFERENCIA,
-		MENSALIDADE
+
+	private enum Operacao {
+		SAQUE, DEPOSITO, TRANSFERENCIA, MENSALIDADE
 	}
-	
+
 	public void exibirSaldo() {
-		
+
 	}
 
 	public void sacar(float valor) {
 
 	}
-
+	
+	/**
+	 * Metodo não será implementado (a principio)
+	 * @param valor
+	 */
 	public void depositar(float valor) {
 
 	}
 
 	public void pagarMensalidade() {
-		
+
 	}
-	
+
 	public void transferir(Conta conta, float valor) {
-		
+
 	}
-	
+
 	public void inativarConta() {
-		
+
 	}
-	
+
 	public void gravarExtrato(float valor, LocalDateTime dataTransacao, Operacao tipoOperacao) {
-		
+
 	}
-	
+
 	public void exibirExtrato() {
-		
+
 	}
-	
+
 	public void exibirDetalhesConta() {
-		
+
 	}
-	
+
 }
