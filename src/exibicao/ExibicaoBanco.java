cebb3f7ac5d98c04e7b2b0c67e1e21be5d78cedb
Projeto - DEV <lokluis@gmail.com>
2017-12-04 15:38:23 -0200

    Adicionando interfaces para exibicao

---
+++ src/exibicao/ExibicaoBanco.java
@@ -0,0 +1,39 @@
+package exibicao;
+
+import interfaces.UtilsBanco;
+import objects.Cliente;
+import objects.Conta;
+
+public class ExibicaoBanco implements UtilsBanco {
+
+	@Override
+	public void apresentarBanco() {
+
+	}
+
+	@Override
+	public void carregarConta() {
+
+	}
+
+	@Override
+	public void criarUsuario() {
+
+	}
+
+	@Override
+	public void criarConta(Cliente cliente) {
+
+	}
+
+	@Override
+	public void repetirOperacoes(Conta conta) {
+
+	}
+
+	@Override
+	public void finalizarOperacoes(Conta conta) {
+
+	}
+
+}
---
+++ src/interfaces/UtilsBanco.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import objects.Conta;
+import objects.Cliente;
+
+public interface UtilsBanco {
+	
+	public void apresentarBanco();
+
+	public void carregarConta();
+
+	public void criarUsuario();
+
+	public void criarConta(Cliente cliente);
+
+	public void repetirOperacoes(Conta conta);
+
+	public void finalizarOperacoes(Conta conta);
+
+}
